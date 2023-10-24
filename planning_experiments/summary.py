e39f9be6362743c0da7063b27e485300ae5574d5
MattiaTanchis <m.tanchis001@studenti.unibs.it>
2023-10-24 16:30:24 +0200

    modifiche

--- planning_experiments/summary.py
+++ planning_experiments/summary.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 import re
-from planning_experiments.constants import STDE, NUM_SOLUTIONS, SOLUTIONS
+from planning_experiments.constants import STDE, NUM_SOLUTIONS, SOLUTIONS,SEARCH_ENGINE,HEURISTIC
 
 SYS = 'planner'
 DOM = 'domain'
@@ -12,6 +12,8 @@ CT = 'comptime'
 SOL = 'solved'
 PL = 'quality'
 NA = 'n/a'
+SE ="search_engine"
+H="heuristic"
 
 
 def extract_float(text: str, regex: str):
@@ -32,9 +34,13 @@ def create_summary(blob_path, output_path):
         for domain in blob[planner].keys():
             for instance in blob[planner][domain].keys():
                 record = {SYS: planner, DOM: domain, PROB: instance}
+                record[SE]=blob[planner][domain][instance][SEARCH_ENGINE]
+                record[H]=blob[planner][domain][instance][HEURISTIC]
                 record[RT] = extract_float(blob[planner][domain][instance][STDE], r'Total Runtime: (.*)\n')
                 record[SOL] = False
                 record[PL] = NA
+                
+                
 
                 if blob[planner][domain][instance][NUM_SOLUTIONS] > 0:
                     record[SOL] = True
